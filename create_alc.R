fcd5f48b1ecb61fb18c63f443b3b621f8e455685
Jernström <laurajer@ad.helsinki.fi>
2020-11-12 14:34:18 +0200

    R code file

---
+++ create_alc.R
@@ -0,0 +1,8 @@
+# Laura Jernström
+# 12/11/2020
+# Data used is Student Performance Data received from 
+# UCI Machine Learning Repository on 12/11/2020,
+# (at https://archive.ics.uci.edu/ml/datasets/Student+Performance).
+
+
+
