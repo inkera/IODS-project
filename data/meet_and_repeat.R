1475a436524b929ba47f3e7b972277da7d96b0d6
Jernström <laurajer@ad.helsinki.fi>
2020-12-02 12:01:09 +0200

    ex 6

---
+++ data/meet_and_repeat.R
@@ -0,0 +1,7 @@
+# Laura Jernström
+# 02/12/2020
+
+# Data Sources: 
+# https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt
+# https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt
+
